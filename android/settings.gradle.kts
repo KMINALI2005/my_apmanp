836a26b74ca5a44a0552161275a070ebc956c76c
KMINALI2005 <162717637+KMINALI2005@users.noreply.github.com>
2025-08-03 12:24:28 +0300

    Update settings.gradle.kts

--- android/settings.gradle.kts
+++ android/settings.gradle.kts
@@ -1,16 +1,18 @@
 pluginManagement {
-    includeBuild("../packages/flutter_tools/gradle")
     repositories {
+        gradlePluginPortal()
         google()
         mavenCentral()
-        gradlePluginPortal()
     }
 }
 
-plugins {
-    id("dev.flutter.flutter-plugin-loader") version "1.0.0"
-    id("com.android.application") version "8.1.0" apply false
-    id("org.jetbrains.kotlin.android") version "1.9.10" apply false
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
 }
 
+rootProject.name = "my_apmanp"
 include(":app")
